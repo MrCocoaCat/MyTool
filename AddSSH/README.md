c4cebe65b9cf72edb8c24c6c6cf27b9956aa451c
MrCocoaCat <liyubo_liyubo@163.com>
2018-11-24 21:08:23 +0800

    add AddSSH
    添加为远程添加ssh 密钥的脚本

---
+++ AddSSH/README.md
@@ -0,0 +1,20 @@
+### 概述
+为远程主机添加ssh密钥，需提供用户名密码
+* 脚本add.sh
+* 地址文件list.txt
+使用前需要安装exect
+```
+apt-get install expect
+```
+### 使用方式
+将add.sh批处理脚本与配置文件list.txt 放置在同一目录下。
+list.txt为需要添加的IP地址，该列表中用户名密码均相同
+### 参数
+
+改脚本需要两个参数
+分别为远程主机的账号及密码
+
+>示例
+```
+sh add.sh user pass
+```
---
+++ AddSSH/add.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+HOSTS=`cat list.txt`
+#echo $HOSTS
+UserName=$1
+Passwd=$2
+for host in $HOSTS
+do
+        echo $HOSTS
+        expect -c "
+            set timeout 5;
+            spawn ssh-copy-id -i /root/.ssh/id_rsa.pub ${UserName}@${host}
+            expect {
+                                \"yes/no\" { send \"yes\r\"; exp_continue }
+                \"*assword\" { send \"${Passwd}\r\" }
+
+             } ;
+            expect eof
+        "
+done
